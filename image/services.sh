757604ea53c6699523478cfea71ccad28c8361bb
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-26 21:40:58 +0200

    clamsmtpD

--- image/services.sh
+++ image/services.sh
@@ -19,6 +19,6 @@ cp /build/runit/freshclam /etc/service/freshclam/run
 sed -i 's/^LogSyslog false.*/LogSyslog true' /etc/clamav/freshclam.conf
 
 ## Setting up clamsmtp
-mkdir /etc/service/clamsmtp
-cp /build/runit/clamsmtp /etc/service/clamsmtp/run
+mkdir /etc/service/clamsmtpd
+cp /build/runit/clamsmtpd /etc/service/clamsmtpd/run
 mkdir /var/run/clamsmtp && chown clamsmtp:clamsmtp /var/run/clamsmtp
