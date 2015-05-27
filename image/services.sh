ed42c11f685ce0dd2f3f20cc4414840de4d79ff9
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-27 12:29:11 +0200

    fixed services, clamd initialises freshclam database, freshclam waits for clamd to start

--- image/services.sh
+++ image/services.sh
@@ -8,17 +8,21 @@ $minimal_apt_get_install clamav-daemon clamav-freshclam clamsmtp
 
 ## Setting up clamd
 mkdir /etc/service/clamd
-cp /build/runit/spamd /etc/service/spamd/run
-sed -i 's/^LogSyslog false.*/LogSyslog true' /etc/clamav/clamd.conf
-sed -i 's/^Foreground false/Foreground true/' /etc/clamav/clamd.conf
+cp /build/runit/clamd /etc/service/clamd/run
+sed -i 's|^LogSyslog.*|LogSyslog true|' /etc/clamav/clamd.conf
+sed -i 's|^Foreground.*|Foreground true|' /etc/clamav/clamd.conf
 mkdir /var/run/clamav && chown clamav:clamav /var/run/clamav
 
 ## Setting up freshclam
 mkdir /etc/service/freshclam
 cp /build/runit/freshclam /etc/service/freshclam/run
-sed -i 's/^LogSyslog false.*/LogSyslog true' /etc/clamav/freshclam.conf
+sed -i 's|^LogSyslog.*|LogSyslog true|' /etc/clamav/freshclam.conf
+sed -i 's|^Foreground.*|Foreground true|' /etc/clamav/freshclam.conf
 
 ## Setting up clamsmtp
 mkdir /etc/service/clamsmtpd
 cp /build/runit/clamsmtpd /etc/service/clamsmtpd/run
 mkdir /var/run/clamsmtp && chown clamsmtp:clamsmtp /var/run/clamsmtp
+sed -i 's|^Listen.*|Listen: 10025|' /etc/clamsmtpd.conf
+sed -i 's|^OutAddress.*|OutAddress: 10026|' /etc/clamsmtpd.conf
+echo 'Action: pass' >> /etc/clamsmtpd.conf
